c055d6a96936c794bd61107c7233eebea15dbd95
Merzouk HAMRANI <merzouk.hamrani@gmail.com>
2018-10-08 00:46:50 +0200

    Add a spring context module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,6 @@ module com.java9.example.demo {
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires java.sql;
+    requires spring.context;
     opens com.java9.example.demo to spring.core;
 }
